ca9e9e833dec0337c540f720096be9ca7345e4f7
h4ck3r-04 <mr.rudradityathakur@gmail.com>
2025-01-09 11:20:10 +0530

    updated recipes for insert many, update one, update many, to fix update many function

--- recipes/update-many.pl
+++ recipes/update-many.pl
@@ -3,40 +3,27 @@ use warnings;
 use Pongo::Client;
 use Pongo::BSON;
 
-my $client = Pongo::Client::client_new("mongodb://localhost:27017");
+my $client = PongoClient->new("mongodb://localhost:27017");
 
-my $collection = Pongo::Client::client_get_collection($client, "testdb", "myCollection");
-
-my @updates = (
-    { name => "Eric Doe", age => 46, email => "eric.updated\@gmail.com" },
-    { name => "Bob Johnson", age => 30, email => "bob.johnson\@example.com" },
-);
-
-foreach my $data (@updates) {
-    my $selector = Pongo::BSON::new();
-    Pongo::BSON::append_utf8($selector, "name", -1, $data->{name}, -1);
-
-    my $update = Pongo::BSON::new();
-    my $set = Pongo::BSON::new();
-    Pongo::BSON::append_int32($set, "age", -1, $data->{age});
-    Pongo::BSON::append_utf8($set, "email", -1, $data->{email}, -1);
-
-    Pongo::BSON::append_document($update, "\$set", -1, $set);
-
-    my $result = Pongo::Client::collection_update_one($collection, $selector, $update, undef, undef, undef);
-
-    if ($result) {
-        print "Document for $data->{name} updated successfully\n";
-    } else {
-        print "Failed to update document for $data->{name}\n";
+my $updates = [
+    {
+        selector => {name => "Alice"},
+        update => {"$set" => {age => 32}},
+    },
+    {
+        selector => {name => "Bob"},
+        update => {"$set" => {age => 45}},
     }
+];
 
-    Pongo::BSON::destroy($selector);
-    Pongo::BSON::destroy($update);
-    Pongo::BSON::destroy($set);
+my $results = $client->update_many("test", "myCollection", $updates);
+
+foreach my $result (@$results) {
+    if ($result) {
+        print "Document updated successfully!\n";
+    } else {
+        print "Failed to update document.\n";
+    }
 }
-
-Pongo::Client::collection_destroy($collection);
-Pongo::Client::client_destroy($client);
 
 1;
--- recipes/update-one.pl
+++ recipes/update-one.pl
@@ -3,32 +3,22 @@ use warnings;
 use Pongo::Client;
 use Pongo::BSON;
 
-my $client = Pongo::Client::client_new("mongodb://localhost:27017");
+my $client = PongoClient->new("mongodb://localhost:27017");
 
-my $collection = Pongo::Client::client_get_collection($client, "testdb", "myCollection");
+my $selector = PongoBSON->new();
+$selector->append_utf8("name", "Alice");
 
-my $selector = Pongo::BSON::new();
-Pongo::BSON::append_utf8($selector, "name", -1, "Eric Doe", -1);
+my $update = PongoBSON->new();
+my $set = PongoBSON->new();
+$set->append_int32("age", 31);
+$update->append_document("\$set", $set);
 
-my $update = Pongo::BSON::new();
-my $set = Pongo::BSON::new();
-Pongo::BSON::append_int32($set, "age", -1, 46);
-Pongo::BSON::append_utf8($set, "email", -1, "eric.updated\@gmail.com", -1);
-
-Pongo::BSON::append_document($update, "\$set", -1, $set);
-
-my $result = Pongo::Client::collection_update_one($collection, $selector, $update, undef, undef, undef);
+my $result = $client->update_one("testdb", "myCollection", $selector, $update);
 
 if ($result) {
-    print "Document updated successfully\n";
+    print "Document updated successfully!\n";
 } else {
     print "Failed to update document\n";
 }
-
-Pongo::BSON::destroy($selector);
-Pongo::BSON::destroy($update);
-Pongo::BSON::destroy($set);
-Pongo::Client::collection_destroy($collection);
-Pongo::Client::client_destroy($client);
 
 1;
